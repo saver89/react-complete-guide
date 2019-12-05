a7f82c2d5889346032f5f824932e2b672e545048
Vladimir <neelkim1989@gmail.com>
2019-12-06 01:48:23 +0500

    add media queries into app

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import './Component/UserOutput/UserOutput.css';
 import ValidationComponent from './Component/ValidationComponent';
 import CharComponent from './Component/CharComponent/CharComponent'
 import './Component/CharComponent/CharComponent.css'
-import Radium from 'radium';
+import Radium, {StyleRoot} from 'radium';
+import Task from './Component/Task/Task'
 
 class App extends Component {
   state = {
@@ -58,26 +59,21 @@ class App extends Component {
     }
 
     return (
-      <div className="App">
-        <div style={centerStyle}>
-          <button style={buttonStyle} onClick={this.toggleShowPershonHandler}>Push me</button>
+      <StyleRoot>
+        <div className="App">
+          <div style={centerStyle}>
+            <button style={buttonStyle} onClick={this.toggleShowPershonHandler}>Push me</button>
+          </div>
+          {this.state.showText ? <Task/> : null}
+          <p>Hint: Keep in mind that JavaScript strings are basically arrays!</p>
+          <hr />    
+          
+          <input onChange={(event)=>this.onChangeHandler(event)} value={this.state.strValue}/>
+          <label>{this.state.strValue}</label>
+          <ValidationComponent length={this.state.strValue.length}></ValidationComponent>
+          {charComponentList}
         </div>
-        {this.state.showText ? <ol>
-          <li>Create an input field (in App component) with a change listener which outputs the length of the entered text below it (e.g. in a paragraph).</li>
-          <li>Create a new component (=> ValidationComponent) which receives the text length as a prop</li>
-          <li>Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length)</li>
-          <li>Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).</li>
-          <li>Render a list of CharComponents where each CharComponent receives a different letter of the entered text (in the initial input field) as a prop.</li>
-          <li>When you click a CharComponent, it should be removed from the entered text.</li>
-        </ol> : null}
-        <p>Hint: Keep in mind that JavaScript strings are basically arrays!</p>
-        <hr />    
-        
-        <input onChange={(event)=>this.onChangeHandler(event)} value={this.state.strValue}/>
-        <label>{this.state.strValue}</label>
-        <ValidationComponent length={this.state.strValue.length}></ValidationComponent>
-        {charComponentList}
-      </div>
+      </StyleRoot>
     )   
   };
 }
