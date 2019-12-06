ecd2efb7366b44442b9ac35661881320b4c39db7
Vladimir <neelkim1989@gmail.com>
2019-12-06 23:56:47 +0500

    add error boundary

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import classes from './App.css';
 import UserInput from './Component/UserInput/UserInput';
 import UserOutput from './Component/UserOutput/UserOutput';
 import './Component/UserOutput/UserOutput.css';
@@ -7,6 +7,7 @@ import ValidationComponent from './Component/ValidationComponent';
 import CharComponent from './Component/CharComponent/CharComponent'
 import './Component/CharComponent/CharComponent.css'
 import Task from './Component/Task/Task'
+import ErrorBoundary from './Component/ErrorBoundary/ErrorBoundary'
 
 class App extends Component {
   state = {
@@ -15,57 +16,54 @@ class App extends Component {
   };
 
   onChangeHandler = (event) => {
-    this.setState({"strValue": event.target.value});
+    this.setState({ "strValue": event.target.value });
   };
 
   toggleShowPershonHandler = () => {
-    this.setState({showText: !this.state.showText});
+    const randomNumber = Math.random();
+    if (randomNumber > 0.7) throw Error('ERROR');
+
+    this.setState({ showText: !this.state.showText });
   };
 
   deleteCharHandler = (index) => {
     const tempStr = this.state.strValue.split('');
     tempStr.splice(index, 1);
     const updatedText = tempStr.join('');
-    this.setState({strValue: updatedText});
-  };  
+    this.setState({ strValue: updatedText });
+  };
 
-  render () {
+  render() {
     const charComponentList = this.state.strValue.split('').map((ch, index) => {
       return <CharComponent letter={ch} key={index} clicked={() => this.deleteCharHandler(index)}></CharComponent>
     });
 
-    const buttonStyle = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      paddint: '8px',
-      cursor: 'pointer',
-    };
-    
     const centerStyle = {
       width: '100%', display: 'flex', justifyContent: 'center'
     };
 
+    let btnClass = null;
     if (this.state.showText) {
-      buttonStyle.backgroundColor = "red";
+      btnClass = classes.red;
     }
 
     return (
-      <div className="App">
-        <div style={centerStyle}>
-          <button style={buttonStyle} onClick={this.toggleShowPershonHandler}>Push me</button>
+      <ErrorBoundary>
+        <div className={classes.App}>
+          <div style={centerStyle}>
+            <button className={btnClass} onClick={this.toggleShowPershonHandler}>Push me</button>
+          </div>
+          {this.state.showText ? <Task /> : null}
+          <p className={classes.red}>Hint: Keep in mind that JavaScript strings are basically arrays!</p>
+          <hr />
+
+          <input onChange={(event) => this.onChangeHandler(event)} value={this.state.strValue} />
+          <label>{this.state.strValue}</label>
+          <ValidationComponent length={this.state.strValue.length}></ValidationComponent>
+          {charComponentList}
         </div>
-        {this.state.showText ? <Task/> : null}
-        <p>Hint: Keep in mind that JavaScript strings are basically arrays!</p>
-        <hr />    
-        
-        <input onChange={(event)=>this.onChangeHandler(event)} value={this.state.strValue}/>
-        <label>{this.state.strValue}</label>
-        <ValidationComponent length={this.state.strValue.length}></ValidationComponent>
-        {charComponentList}
-      </div>
-    )   
+      </ErrorBoundary>
+    )
   };
 }
 
