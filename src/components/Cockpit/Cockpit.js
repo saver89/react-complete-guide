cf7988e266453d5aa3c47560bb573115b9f2f235
Vladimir <neelkim1989@gmail.com>
2019-12-10 02:24:08 +0500

    add useState hook into Cockpit.js

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classes from './Cockpit.css'
 
-const cockpit = (props) => {
+const Cockpit = (props) => {
+    useEffect(() => {
+        console.log('[Cockpit.js] useEffect');
+    });
 
     let btnClass = null;
 
@@ -17,4 +20,4 @@ const cockpit = (props) => {
     );
 };
 
-export default cockpit;
+export default Cockpit;
