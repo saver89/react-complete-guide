36a2d847ecd5166e3d7d940b4eca53320121af3d
Vladimir <neelkim1989@gmail.com>
2019-12-27 03:30:56 +0500

    start burger builder

--- src/components/Study/Persons/Person/Person.js
+++ src/components/Study/Persons/Person/Person.js
@@ -6,7 +6,8 @@ class Person extends Component {
         console.log('[Person.js] rendering...');
 
         return (<div onClick={this.props.personClickHandler} className={classes.Person}>
-            <input onChange={this.props.nameChangeHandler} type="text" value={this.props.name}/>
+            <input onChange={this.props.nameChangeHandler} type="text" value={this.props.name} 
+                onClick={(e) => { e.stopPropagation(); }} />
             <p>{this.props.name}</p>
             <p>{this.props.age}</p>
         </div>);
