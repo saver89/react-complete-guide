52da085199b431ea25490d9a9198d66891c678d4
Vladimir <neelkim1989@gmail.com>
2020-02-01 10:34:19 +0500

    fetch and output orders in orders page

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,11 +1,36 @@
-import React from 'react';
-import classes from './Order.css'
+import React from "react";
+import classes from "./Order.css";
 
-const Order = (props) => (
+const Order = props => {
+  const ingredients = [];
+  for (let ingrName in props.ingredients) {
+    ingredients.push({ name: ingrName, amount: props.ingredients[ingrName] });
+  }
+
+  const ingrOutput = ingredients.map(ig => {
+    return (
+      <span
+        style={{
+          textTransform: "capitalize",
+          display: "inline-block",
+          margin: "0 8px",
+          border: "1px solid #ccc",
+          padding: "5px"
+        }}
+        key={ig.name}
+      >
+        {ig.name} ({ig.amount})
+      </span>
+    );
+  });
+  return (
     <div className={classes.Order}>
-        <p>Ingredients: Salad (1)</p>
-        <p>Price: <strong>USD 5.45</strong></p>
+      <p>Ingredients: {ingrOutput}</p>
+      <p>
+        Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong>
+      </p>
     </div>
-);
+  );
+};
 
 export default Order;
