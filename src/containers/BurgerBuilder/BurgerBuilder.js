a02d2385bc9cc7a447cbd378bfd9b787c41d5fbc
Vladimir <neelkim1989@gmail.com>
2020-02-29 00:42:45 +0500

    support ingredient amount change

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -29,14 +29,14 @@ class BurgerBuilder extends Component {
     }
 
     componentDidMount() {
-        console.log(this.props);
-        axios.get('/ingredients.json')
-            .then((response) => {
-                this.setState({ ingredients: response.data });
-            })
-            .catch(error => {
-                this.setState({error: true});
-            });
+        // console.log(this.props);
+        // axios.get('/ingredients.json')
+        //     .then((response) => {
+        //         this.setState({ ingredients: response.data });
+        //     })
+        //     .catch(error => {
+        //         this.setState({error: true});
+        //     });
     }
 
     updatePurchaseState = (ingr) => {
